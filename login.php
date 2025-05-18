2782a6a00453841de583f8729c1a735e187cdc89
Yasin Gur <yasingur657@gmail.com>
2025-05-18 16:52:10 +0300

    giriş sayfası yapıldı

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+// filepath: c:\Users\yasin\Desktop\Web-Proje\login.php
+// Oturum baslat
+session_start();
+
+// Form gonderildi mi kontrol et
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formdan gelen e-posta ve sifre bilgilerini al
+    $eposta = $_POST["eposta"];
+    $sifre = $_POST["sifre"];
+    
+    // E-posta adresinden kullanici adini cikar (@ isaretinden onceki kisim)
+    $kullaniciAdi = explode('@', $eposta)[0];
+    
+    // Sifre, e-postanin bas kismiyla eslesiyor mu ve e-posta sakarya.edu.tr domain'ine sahip mi kontrol et
+    if ($sifre === $kullaniciAdi && strpos($eposta, "@sakarya.edu.tr") !== false) {
+        // Gecerli giris
+        $_SESSION["kullanici"] = $eposta;
+        $_SESSION["kullaniciAdi"] = $kullaniciAdi;
+        
+        // Hos geldin mesaji ile about.html sayfasina yonlendir
+        header("Location: about.html?hosgeldin=" . $kullaniciAdi);
+        exit();
+    } else {
+        // Gecersiz giris
+        // Hata mesajiyla giris sayfasina geri yonlendir
+        header("Location: login.html?hata=1");
+        exit();
+    }
+} else {
+    // Eger biri POST verisi olmadan bu sayfaya direkt erismeye calisirsa
+    header("Location: login.html");
+    exit();
+}
+?>
